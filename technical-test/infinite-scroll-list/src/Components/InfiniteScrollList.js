5421e656e24edf5ac40f43fa0a25a89fafde6e7a
Daniel Rojas <danni3231@hotmail.com>
2025-02-11 01:03:55 -0500

    Fixed structure

--- technical-test/infinite-scroll-list/src/Components/InfiniteScrollList.js
+++ technical-test/infinite-scroll-list/src/Components/InfiniteScrollList.js
@@ -1,26 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
+import Loader from "./Loader";
 import Card from "./Card";
 import "./InfiniteScrollList.js";
 
+const InfiniteScrollList = ({ fetchItems }) => {
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
 
+  // Initial load and more data when making scroll
+  const loadMore = async () => {
+    if (loading || !hasMore) return;
+    setLoading(true);
 
-const InfiniteScrollList = ({ data }) => {
+    const newItems = await fetchItems();
+    setItems((prevItems) => [...prevItems, ...newItems]);
+    setHasMore(newItems.length > 0); // If new elements are not obtained, deactivate `hasmore`
+    setLoading(false);
+  };
 
-  
-    return (
-        <div className='infinite_scroll_list'>
-            {data.map((user, index) => {
-                return (
-                    <Card
-                        key={index}
-                        image={user.image}
-                        name={user.name}
-                    />
-                );
-            })}
-        </div>
-    );
+  // Detects the scroll at the end of the page
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop >=
+      document.documentElement.offsetHeight - 100
+    ) {
+      loadMore();
+    }
+  };
+
+  useEffect(() => {
+    loadMore(); // Initial load
+    window.addEventListener("scroll", handleScroll); // Add the scroll listener
+    return () => window.removeEventListener("scroll", handleScroll); // Clean the listener when disassembling
+  }, []); // Only runs once
+
+  return (
+    <div className="infinite_scroll_container">
+      <div className="infinite_scroll_list">
+        {items.map((user, index) => {
+          return <Card key={index} image={user.image} name={user.name} />;
+        })}
+      </div>
+      {loading && <Loader />}
+      {!hasMore && <p>No hay más elementos</p>}
+    </div>
+  );
 };
 
 export default InfiniteScrollList;
