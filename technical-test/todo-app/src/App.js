42197e641d4c7690791e26b528a9bb9116591490
Daniel Rojas <danni3231@hotmail.com>
2025-02-11 22:46:19 -0500

    added function add task and persistent

--- technical-test/todo-app/src/App.js
+++ technical-test/todo-app/src/App.js
@@ -1,19 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Task from "./Components/Task";
 
 import "./App.css";
 
-const initialTasks = [
-  { id: 1, name: "Tarea 1", status: "pending" },
-  { id: 2, name: "Tarea 2", status: "in-progress" },
-  { id: 3, name: "Tarea 3", status: "completed" },
-];
-
 const statuses = ["pending", "in-progress", "completed"];
 function App() {
-  const [tasks, setTasks] = useState(initialTasks);
+  const [tasks, setTasks] = useState([]);
   const [history, setHistory] = useState([]);
+  const [newTaskName, setNewTaskName] = useState("");
 
+  // Load tasks from localStorage at the beginning
+  useEffect(() => {
+    const savedTasks = JSON.parse(localStorage.getItem("tasks"));
+    if (Array.isArray(savedTasks) && savedTasks.length > 0) {
+      setTasks(savedTasks);
+    } else {
+      setTasks([
+        { id: 1, name: "Mejorar Perfomance", status: "pending" },
+        { id: 2, name: "Agregar Timer", status: "in-progress" },
+        { id: 3, name: "Agregar Redux", status: "completed" },
+      ]);
+    }
+  }, []);
+
+  // Save tasks in localStorage when task change
+  useEffect(() => {
+    if (tasks.length > 0) {
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+  }, [tasks]);
+
+  // drag & drop functions
   const onDragStart = (e, taskId) => {
     e.dataTransfer.setData("taskId", taskId);
   };
@@ -24,7 +41,7 @@ function App() {
       task.id === Number(taskId) ? { ...task, status: newStatus } : task
     );
 
-    // Guardar en el historial
+    // save history
     setHistory((prevHistory) => {
       const newHistory = [...prevHistory, tasks];
       return newHistory.length > 3 ? newHistory.slice(1) : newHistory;
@@ -33,6 +50,7 @@ function App() {
     setTasks(updatedTasks);
   };
 
+  // undo changes
   const undo = () => {
     if (history.length > 0) {
       setTasks(history[history.length - 1]);
@@ -40,12 +58,49 @@ function App() {
     }
   };
 
+  // Add new task
+  const addTask = () => {
+    if (!newTaskName.trim()) return;
+
+    const newTask = {
+      id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+      name: newTaskName,
+      status: "pending",
+    };
+
+    setTasks([...tasks, newTask]);
+    setNewTaskName("");
+  };
+
   return (
     <div className="App">
       <h1>Aplicación ToDo</h1>
-      <button className="btn btn-back" onClick={undo} disabled={history.length === 0}>
-        Deshacer
-      </button>
+      <div className="inputs-container">
+        <div className="input">
+          <label>Añadir nueva tarea</label>
+          <input
+            type="text"
+            value={newTaskName}
+            onChange={(e) => setNewTaskName(e.target.value)}
+            placeholder="Escribe el nombre de la tarea"
+          />
+          <button onClick={addTask} className="btn">
+            Agregar
+          </button>
+        </div>
+
+        <div className="input">
+          <label>Deshacer ultimo cambio</label>
+          <button
+            className="btn btn-back"
+            onClick={undo}
+            disabled={history.length === 0}
+          >
+            Deshacer
+          </button>
+        </div>
+      </div>
+
       <div className="tasksboard">
         {statuses.map((status) => (
           <div
